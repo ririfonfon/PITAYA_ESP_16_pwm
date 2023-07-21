30016a39e370c1da285fe02b55af4232816b1e9b
richard fontaine <ririfonfon10@gmail.com>
2023-07-21 10:25:59 +0200

    Update rtc_clock.h

--- src/rtc_clock.h
+++ src/rtc_clock.h
@@ -1,18 +1,25 @@
 #ifndef rtc_clock
 #define rtc_clock
 
-#include <Wire.h> 
-#include <RtcDS3231.h>
-#include <EepromAT24C32.h>
+#include <Wire.h>
+// #include <RtcDS3231.h>
+// #include <EepromAT24C32.h>
+#include "M5_I2C_BM8563.h"
+#include "RtcUtility.h"
+#include "RtcDateTime.h"
 
-RtcDS3231<TwoWire> Rtc(Wire);
-EepromAt24c32<TwoWire> RtcEeprom(Wire);
+I2C_BM8563 Rtc;
+I2C_BM8563_DateTypeDef Rtcdate;
+I2C_BM8563_TimeTypeDef Rtctime;
+
+// RtcDS3231<TwoWire> Rtc(Wire);
+// EepromAt24c32<TwoWire> RtcEeprom(Wire);
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
 char DaysOfWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-DS3231AlarmOne alarm_one = Rtc.GetAlarmOne();
+// DS3231AlarmOne alarm_one = Rtc.GetAlarmOne();
 
 void printDateTime(const RtcDateTime &dt)
 {
@@ -37,10 +44,12 @@ void init_clock()
 {
     //--------RTC SETUP ------------
 
-    Rtc.Begin(21, 22); // the available pins for SDA, SCL
-    RtcEeprom.Begin();
+    Rtc.begin(); // the available pins for SDA, SCL
+    // Rtc.Begin(21, 22); // the available pins for SDA, SCL
+    // RtcEeprom.Begin();
 
     RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+    RtcDateTime now;
 
 #ifdef DEBUG
     Serial.print("compiled : ");
@@ -49,59 +58,47 @@ void init_clock()
     Serial.println();
 #endif
 
-    if (!Rtc.IsDateTimeValid())
+    if (!Rtc.isEnable())
     {
-        if (Rtc.LastError() != 0)
-        {
-            // we have a communications error
-            // see https://www.arduino.cc/en/Reference/WireEndTransmission for
-            // what the number means
-            Serial.print("RTC communications error = ");
-            Serial.println(Rtc.LastError());
-        }
-        else
-        {
-            Serial.println("RTC lost confidence in the DateTime!");
-            Rtc.SetDateTime(compiled);
-        }
+        Serial.print("RTC communications error ");
     }
-
-    if (!Rtc.GetIsRunning())
+    else if (Rtc.isEnable())
     {
-        Serial.println("RTC was not actively running, starting now");
-        Rtc.SetIsRunning(true);
-    }
+        Rtc.getDate(&Rtcdate);
+        Rtc.getTime(&Rtctime);
 
-    RtcDateTime now = Rtc.GetDateTime();
+        now = RtcDateTime(Rtcdate.month + ' ' + Rtcdate.date + ' ' + Rtcdate.year, Rtctime.hours + ':' + Rtctime.minutes + ':' + Rtctime.seconds);
+
 #ifdef DEBUG
-    Serial.print("now : ");
-    Serial.print("\t\t");
-    printDateTime(now);
-    Serial.println();
+        Serial.print("now : ");
+        Serial.print("\t\t");
+        printDateTime(now);
+        Serial.println();
 
-    Serial.print("Pure now : ");
-    Serial.print("\t\t");
-    Serial.print(now.TotalSeconds());
-    Serial.println();
-    Serial.print("Pure compiled : ");
-    Serial.print("\t");
-    Serial.print(compiled.TotalSeconds());
-    Serial.println();
+        Serial.print("Pure now : ");
+        Serial.print("\t\t");
+        Serial.print(now.TotalSeconds());
+        Serial.println();
+        Serial.print("Pure compiled : ");
+        Serial.print("\t");
+        Serial.print(compiled.TotalSeconds());
+        Serial.println();
 #endif
 
-    if (now < compiled)
-    {
-        Serial.println("RTC is older than compile time!  (Updating DateTime)");
-        Rtc.SetDateTime(compiled);
+        if (now < compiled)
+        {
+            Serial.println("RTC is older than compile time!  (Updating DateTime)");
+            Rtc.SetDateTime(compiled);
+        }
+
+        // never assume the Rtc was last configured by you, so
+        // just clear them to your needed state
+        // Rtc.Enable32kHzPin(false);
+        // Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+
+        /* comment out on a second run to see that the info is stored long term */
+        // Store something in memory on the Eeprom
     }
-
-    // never assume the Rtc was last configured by you, so
-    // just clear them to your needed state
-    // Rtc.Enable32kHzPin(false);
-    // Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
-
-    /* comment out on a second run to see that the info is stored long term */
-    // Store something in memory on the Eeprom
 }
 
 void alarm_set()
@@ -117,7 +114,6 @@ void alarm_set()
         0,
         DS3231AlarmOneControl_HoursMinutesSecondsMatch);
     Rtc.SetAlarmOne(alarm1);
-
 }
 
 #endif
