4d6f2b274d4d0a5cdbab66a08bc6265fddff68be
richard fontaine <ririfonfon10@gmail.com>
2023-07-23 11:13:40 +0200

    Update rtc_clock.h

--- src/rtc_clock.h
+++ src/rtc_clock.h
@@ -21,52 +21,6 @@ char DaysOfWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"
 
 // DS3231AlarmOne alarm_one = Rtc.GetAlarmOne();
 
-void Rtc_SetDateTime(RtcDateTime _datetime)
-{
-    Rtctime.hours = _datetime.Hour(); // Set the rtc clock time.
-    Rtctime.minutes = _datetime.Minute();
-    Rtctime.seconds = _datetime.Second();
-
-    Rtcdate.weekDay = _datetime.DayOfWeek(); // Set the rtc clock date.
-    Rtcdate.month = _datetime.Month();
-    Rtcdate.date = _datetime.Day();
-    Rtcdate.year = _datetime.Year();
-
-    Rtc.setTime(&Rtctime); // Example Synchronize the set time to the rtc.
-    Rtc.setDate(&Rtcdate); // Synchronize the set date to the rtc
-}
-
-RtcDateTime Rtc_GetDateTime()
-{
-    Rtc.getDate(&Rtcdate);
-    Rtc.getTime(&Rtctime);
-
-    char *monthchar, *datechar, *yearchar, *hourschar, *minuteschar, *secondschar, *datertc, *timertc;
-    const char *space = " ";
-    const char *dopo = ":";
-    strchr(monthchar, Rtcdate.month);
-    strchr(datechar, Rtcdate.date);
-    strchr(yearchar, Rtcdate.year);
-    strchr(hourschar, Rtctime.hours);
-    strchr(minuteschar, Rtctime.minutes);
-    strchr(secondschar, Rtctime.seconds);
-
-    strcpy(datertc, monthchar);
-    strcat(datertc, space);
-    strcat(datertc, datechar);
-    strcat(datertc, space);
-    strcat(datertc, yearchar);
-
-    strcpy(timertc, hourschar);
-    strcat(timertc, dopo);
-    strcat(timertc, minuteschar);
-    strcat(timertc, dopo);
-    strcat(timertc, secondschar);
-
-    RtcDateTime Result = RtcDateTime(datertc, timertc);
-    return Result;
-}
-
 void printDateTime(const RtcDateTime &dt)
 {
     char datestring[20];
@@ -86,11 +40,52 @@ void printDateTime(const RtcDateTime &dt)
     Serial.print(datestring);
 }
 
+void printDateTimeBM(I2C_BM8563_DateTypeDef _rtcdate, I2C_BM8563_TimeTypeDef _rtctime)
+{
+    Serial.printf("date %04d/%02d/%02d time %02d:%02d:%02d day of week %01d\n",
+                  _rtcdate.year,
+                  _rtcdate.month,
+                  _rtcdate.date,
+                  _rtctime.hours,
+                  _rtctime.minutes,
+                  _rtctime.seconds,
+                  _rtcdate.weekDay);
+}
+
+void Rtc_SetDateTime(RtcDateTime _datetime)
+{
+    Rtctime.hours = _datetime.Hour(); // Set the rtc clock time.
+    Rtctime.minutes = _datetime.Minute();
+    Rtctime.seconds = _datetime.Second();
+
+    Rtcdate.weekDay = _datetime.DayOfWeek(); // Set the rtc clock date.
+    Rtcdate.month = _datetime.Month();
+    Rtcdate.date = _datetime.Day();
+    Rtcdate.year = _datetime.Year();
+
+    Rtc.setTime(&Rtctime); // Synchronize the set time to the rtc.
+    Rtc.setDate(&Rtcdate); // Synchronize the set date to the rtc
+}
+
+RtcDateTime Rtc_GetDateTime()
+{
+    Rtc.getDate(&Rtcdate);
+    Rtc.getTime(&Rtctime);
+    String datertc, timertc;
+    datertc = String(Rtcdate.month) + " " + String(Rtcdate.date) + " " + String(Rtcdate.year);
+    timertc = String(Rtctime.hours) + ":" + String(Rtctime.minutes) + ":" + String(Rtctime.seconds);
+    RtcDateTime Result = RtcDateTime(datertc.c_str(), timertc.c_str());
+    return Result;
+}
+
 void init_clock()
 {
     //--------RTC SETUP ------------
 
+    Wire.begin(21, 22, 100000UL);
+
     Rtc.begin(); // the available pins for SDA, SCL
+
     // Rtc.Begin(21, 22); // the available pins for SDA, SCL
     // RtcEeprom.Begin();
 
@@ -149,6 +144,14 @@ void alarm_set()
     //     0,
     //     DS3231AlarmOneControl_HoursMinutesSecondsMatch);
     // Rtc.SetAlarmOne(alarm1);
+
+    I2C_BM8563_TimeTypeDef RtcAlarm;
+
+    RtcAlarm.hours = time_on_hour;
+    RtcAlarm.minutes = time_on_minute;
+    RtcAlarm.seconds = 0;
+
+    Rtc.SetAlarmIRQ(RtcAlarm);
 }
 
 #endif
